b6d202e8fe54a2cb77b5a057e21a6fa0bf92fb8f
Alexander Yakushev <alex@bytopia.org>
2022-07-21 17:12:08 +0300

    Add support for draft-preview posts

--- src/generator/main.clj
+++ src/generator/main.clj
@@ -1,5 +1,8 @@
 (ns generator.main
   (:require [cryogen-core.compiler :as comp]
+            cryogen-core.config
+            cryogen-core.io
+            cryogen-core.markup
             [cryogen-core.plugins :refer [load-plugins]])
   (:import (java.time LocalDateTime ZoneId)
            (java.time.format DateTimeFormatter)
@@ -41,15 +44,53 @@
                (str pre post)))
     article))
 
+(defn update-article [article]
+  (-> article
+      strip-date-from-post-url
+      article-date->date-published))
+
+(defn read-draft-preview-posts [config]
+  (->> (cryogen-core.markup/markups)
+       (mapcat
+        (fn [mu]
+          (->>
+           (comp/find-posts config mu)
+           (pmap #(comp/parse-post % config mu))
+           (filter #(= (:draft %) "preview")))))
+       (sort-by :date)
+       reverse))
+
+(defn compile-draft-preview-posts []
+  (let [{:keys [^String site-url blog-prefix rss-name recent-posts keep-files ignored-files previews? author-root-uri theme]
+         :as   config} (cryogen-core.config/resolve-config)
+        posts (->> (read-draft-preview-posts config)
+                   (map (partial comp/add-description config))
+                   (map update-article)
+                   (remove nil?))
+        params0 (merge
+                 config
+                 {:today         (Date.)
+                  :title         (:site-title config)
+                  :archives-uri  (comp/page-uri "archives.html" config)
+                  :index-uri     (comp/page-uri "index.html" config)
+                  :tags-uri      (comp/page-uri "tags.html" config)
+                  :rss-uri       (cryogen-core.io/path "/" blog-prefix rss-name)
+                  :site-url      (if (.endsWith site-url "/") (.substring site-url 0 (dec (count site-url))) site-url)})
+        params params0]
+    (when-not (empty? posts)
+      (println (text-decoration.core/blue "compiling draft preview posts..."))
+      (comp/compile-posts params posts))))
+
 (defn build* [opts changeset]
-  (comp/compile-assets-timed
-   {:update-article-fn
-    (fn [article _]
-      (when (or (not (:draft article)) (:draft opts))
-        (-> article
-            strip-date-from-post-url
-            article-date->date-published)))}
-   changeset))
+  (let [gen-drafts? (:draft opts)]
+   (comp/compile-assets-timed
+    {:update-article-fn
+     (fn [article _]
+       (when (or gen-drafts? (not (:draft article)))
+         (update-article article)))}
+    changeset)
+   (when-not gen-drafts?
+     (compile-draft-preview-posts))))
 
 (defn build [opts]
   (load-plugins)
